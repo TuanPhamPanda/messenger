4d28abbf1b8e8c0494c530b15211d09d46c91696
AnhTuan <phamtuan12022001@gmail.com>
2024-03-21 01:34:00 +0700

    users-screen-and-conversations-screen-conversation-creation

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,8 +1,13 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 async function UsersLayout({ children }: { children: React.ReactNode }) {
+  const users = await getUsers();
+
   return (
     <Sidebar>
+      <UserList items={users} />
       <div className="h-full">{children}</div>
     </Sidebar>
   );
